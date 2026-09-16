d332773a7e074d699c66a9e299440daf8dd1a983
Wilber_Lancea <202202533@est.umss.edu>
2026-09-16 18:35:27 -0400

    Revert "Implementa interfaz de asignaturas y ambientes"
    This reverts commit 644e21d829ae443db732cd33d60cc2dca537da71.

--- resources/js/componentes/Aplicacion.jsx
+++ resources/js/componentes/Aplicacion.jsx
@@ -1,21 +1,5 @@
-import React from "react";
 import UsuariosRoles from "../paginas/admin/UsuariosRoles.jsx";
-import AsignaturasAmbientes from "../paginas/admin/AsignaturasAmbientes.jsx";
 
 export default function Aplicacion() {
-    const [pagina, setPagina] = React.useState("usuarios");
-
-    if (pagina === "asignaturas") {
-        return (
-            <AsignaturasAmbientes
-                onNavigate={setPagina}
-            />
-        );
-    }
-
-    return (
-        <UsuariosRoles
-            onNavigate={setPagina}
-        />
-    );
+    return <UsuariosRoles />;
 }
--- resources/js/paginas/admin/UsuariosRoles.jsx
+++ resources/js/paginas/admin/UsuariosRoles.jsx
@@ -45,11 +45,10 @@ const usuarios = [
     },
 ];
 
-function UsuariosRoles({ onNavigate }) {
+function UsuariosRoles() {
 
     const [mostrarFormulario, setMostrarFormulario] = React.useState(false);
-    const [usuarioEditando, setUsuarioEditando] = React.useState(null);
-    
+
     const [nuevoUsuario, setNuevoUsuario] = React.useState({
         nombre: "",
         correo: "",
@@ -79,19 +78,9 @@ function UsuariosRoles({ onNavigate }) {
                     <div style={styles.menuTitle}>ADMINISTRADOR</div>
 
                     <MenuItem icon="♙" text="Padrón" />
-
-<MenuItem
-    icon="▤"
-    text="Asignaturas y ambientes"
-    onClick={() => onNavigate("asignaturas")}
-/>
+                    <MenuItem icon="▤" text="Asignaturas y ambientes" />
                     <MenuItem icon="⌗" text="Códigos QR" active />
-                    <MenuItem
-                     icon="♙"
-                     text="Usuarios y roles"
-                     selected
-                     onClick={() => onNavigate("usuarios")}
-                     />
+                    <MenuItem icon="♙" text="Usuarios y roles" selected />
                     <MenuItem icon="▧" text="Bitácora" />
                     <MenuItem icon="↻" text="Respaldo" />
                 </div>
@@ -206,21 +195,9 @@ function UsuariosRoles({ onNavigate }) {
 
                                 {/* ACCIONES */}
                                 <div>
-                                    <button 
-    style={styles.editButton}
-    onClick={() => {
-        setUsuarioEditando(usuario);
-        setNuevoUsuario({
-            nombre: usuario.nombre,
-            correo: usuario.correo,
-            rol: usuario.rol,
-        });
-        setMostrarFormulario(true);
-    }}
->
-    Editar
-</button>
-                                    
+                                    <button style={styles.editButton}>
+                                        Editar
+                                    </button>
                                 </div>
                             </div>
                         ))}
@@ -354,15 +331,15 @@ function UsuariosRoles({ onNavigate }) {
 /* ================================
    COMPONENTE DEL MENU
 ================================ */
-function MenuItem({ icon, text, selected, onClick }) {
+
+function MenuItem({ icon, text, selected }) {
     return (
         <div
-    style={{
-        ...styles.menuItem,
-        ...(selected ? styles.menuSelected : {}),
-    }}
-    onClick={onClick}
->
+            style={{
+                ...styles.menuItem,
+                ...(selected ? styles.menuSelected : {}),
+            }}
+        >
             <span style={styles.menuIcon}>
                 {icon}
             </span>
